6fc5e72d6de92a427b900a4d417681b634ea646b
admin <admin@iMac-de-admin-2.local>
2017-07-19 12:26:19 +0200

    upd version - `date`

--- dist/environments/production.js
+++ dist/environments/production.js
@@ -4,7 +4,7 @@ exports.prodVariables = {
     environmentName: 'Production Environment',
     ionicEnvName: 'prod',
     // Front-end
-    apiEndpoint: 'https://quiet-anchorage-72397.herokuapp.com/',
+    apiEndpoint: 'https://quiet-anchorage-72397.herokuapp.com',
     // Back-end
     dbHost: 'mongodb://ionic_devops:asdfrewq@ds161032.mlab.com:61032',
     dbName: 'ionic_devops_demo'
