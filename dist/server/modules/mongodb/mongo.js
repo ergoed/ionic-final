92d0fefa8daca349b9fb7900bb2b6fe60c31b710
admin <admin@iMac-de-admin-2.local>
2017-07-19 10:04:20 +0200

    add server ignore files

---
+++ dist/server/modules/mongodb/mongo.js
@@ -0,0 +1,28 @@
+"use strict";
+exports.__esModule = true;
+var mongoose = require("mongoose");
+// Import MongoDB config
+var config_1 = require("../../config");
+/*
+  Use TypeScript with mongoose models
+  See https://github.com/Appsilon/styleguide/wiki/mongoose-typescript-models
+  and https://gist.github.com/brennanMKE/ee8ea002d305d4539ef6 for more info about
+  Mongoose Interface & Generic Types declaraton.
+*/
+// Define MongoDB path url
+var MONGODB_URI = process.env.MONGODB_URI || config_1.DB_HOST + "/" + config_1.DB_NAME;
+exports.mongoDbConnect = function () {
+    return new Promise(function (resolve, reject) {
+        // Connect to MongoDB with Mongoose
+        mongoose.connect(MONGODB_URI, {
+            useMongoClient: true
+        }, function (err) {
+            if (err) {
+                reject("Error connecting to MongoDB! -> " + MONGODB_URI);
+            }
+            else {
+                resolve("MongoDB Ready! -> " + MONGODB_URI);
+            }
+        });
+    });
+};
