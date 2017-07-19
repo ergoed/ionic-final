107978b95405408186a6ac71358f4524dceaa0f3
admin <admin@iMac-de-admin-2.local>
2017-07-19 13:02:59 +0200

    upd...

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
 
-var path = YOUR_DEPLOY_PATH;
+var path = "./platforms/browser/www";
 
 gulp.task('deploy', function() {
   return gulp.src( path+'/**/*' )
