16ef2d34e9ed025b7e703f03f0dc177f530d7325
admin <admin@iMac-de-admin-2.local>
2017-07-19 12:01:06 +0200

    asd

---
+++ dist/environments/production.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+exports.prodVariables = {
+    environmentName: 'Production Environment',
+    ionicEnvName: 'prod',
+    // Front-end
+    apiEndpoint: 'https://quiet-anchorage-72397.herokuapp.com/',
+    // Back-end
+    dbHost: 'mongodb://ionic_devops:asdfrewq@ds161032.mlab.com:61032',
+    dbName: 'ionic_devops_demo'
+};
